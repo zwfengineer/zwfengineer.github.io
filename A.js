399722a8585b01fec967eb0f63a4c7b3945d710d
zwf <1358859314@qq.com>
2021-11-12 13:01:51 +0800

    new commit

---
+++ A.js
@@ -0,0 +1,6 @@
+function hvmenu() {
+    var menu = document.getElementsByClassName('menu')[0];
+    var menuico = document.getElementsByClassName('menuico')[0];
+    menu.hidden = (menu.hidden) ? false:true;
+    menuico.hidden = (menuico.hidden) ? false:true;
+}
